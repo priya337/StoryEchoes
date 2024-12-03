1edb5bc97e3af17cb957acdf760eeb7d11baf87a
nehais <nehasinha82@gmail.com>
2024-12-03 20:00:49 +0100

    Neha: Replaced anchor tags to Link in Footer. Added icon for simplicity

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,20 @@
-import React from 'react';
-import '../styles/Footer.css'; 
-import githubLogo from '../assets/GitHub-logo.jpg';
-import backgroundImage from '../assets/back.png';
+import { Link } from "react-router-dom";
+import "../styles/Footer.css";
+import githubLogo from "../assets/GitHub-logo.jpg";
+import openIcon from "../assets/open.png";
+import backgroundImage from "../assets/back.png";
 
 const Footer = () => {
   return (
-    <div className="footer" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <a href="https://github.com/priya337/StoryEchoes.git" target="_blank" rel="noopener noreferrer" className="footer-link">
-        <img 
-          src={githubLogo} 
-          alt="GitHub" 
-          className="github-logo"
-        />
-      </a>
+    <div
+      className="footer"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <img src={githubLogo} alt="GitHub" className="github-logo" />
       <span className="open-link-icon-wrapper">
-        <a href="https://github.com/priya337/StoryEchoes.git" target="_blank" rel="noopener noreferrer" className="open-link-icon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            width="20px"
-            height="20px"
-          >
-            <path d="M13.5 10.5L19 5v4M19 5h-4m4 0v4m-8 8H5V5h4V3H3v18h18v-6h-2v4h-8z" />
-          </svg>
-        </a>
+        <Link to="https://github.com/priya337/StoryEchoes.git">
+          <img src={openIcon} alt="Open Icon" className="open-icon" />
+        </Link>
       </span>
     </div>
   );
