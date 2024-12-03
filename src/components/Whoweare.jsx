79854d703aee85fd0b5ddd4124d0df6051c670a1
nehais <nehasinha82@gmail.com>
2024-12-03 19:47:25 +0100

    Neha: Who we are with Linked In & Git Links

--- src/components/Whoweare.jsx
+++ src/components/Whoweare.jsx
@@ -1,10 +1,12 @@
 // Whoweare.jsx
-import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Whoweare.css"; // Import your CSS for styling
 
 // Make sure the image files are imported
-import Priya from "../assets/dinno.png";  // Import the profile image for Priya
-import Neha from "../assets/dino.png";  // Import the profile image for Neha
+import Priya from "../assets/dinno.png"; // Import the profile image for Priya
+import Neha from "../assets/dino.png"; // Import the profile image for Neha
+import GitLogo from "../assets/GitHub-logo.jpg";
+import LinkedInLogo from "../assets/LinkedInLogo.png";
 
 function Whoweare() {
   return (
@@ -24,6 +26,22 @@ function Whoweare() {
                 <li>
                   <strong>Country:</strong> The Netherlands
                 </li>
+                <li>
+                  <Link to="https://github.com/priya337/">
+                    <img
+                      src={GitLogo}
+                      alt="GitHub Logo"
+                      className="profile-git"
+                    />
+                  </Link>
+                  <Link to="https://linkedin.com/">
+                    <img
+                      src={LinkedInLogo}
+                      alt="LinkedIn Logo"
+                      className="profile-linked"
+                    />
+                  </Link>
+                </li>
               </ul>
             </li>
             <li>
@@ -36,6 +54,22 @@ function Whoweare() {
                 </li>
                 <li>
                   <strong>Country:</strong> Germany
+                </li>
+                <li>
+                  <Link to="https://github.com/nehais/">
+                    <img
+                      src={GitLogo}
+                      alt="GitHub Logo"
+                      className="profile-git"
+                    />
+                  </Link>
+                  <Link to="https://linkedin.com/in/neha-builds">
+                    <img
+                      src={LinkedInLogo}
+                      alt="LinkedIn Logo"
+                      className="profile-linked"
+                    />{" "}
+                  </Link>
                 </li>
               </ul>
             </li>
