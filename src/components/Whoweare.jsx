e2692d0ed1428dc0c59e62c86ce7117f33549e02
Neha <neha@Mac-mini.local>
2024-12-12 11:54:51 -0500

    Neha: max heigh of shelf. Who we are content & css. Max editing page size.

--- src/components/Whoweare.jsx
+++ src/components/Whoweare.jsx
@@ -13,7 +13,11 @@ function Whoweare() {
     <>
       <div className="about-main-content">
         <div className="about-container">
-          <h1>Meet the team!</h1>
+          <h2 className="about-site-title">Story Echoes... Echo your imagination, one story at a time.</h2>
+          <h4 className="about-site-desc">Create, read, and explore stories like never before! This site lets kids craft their own books by doodling covers, adding images, 
+            recording their voices, and even turning their stories into awesome AI art. Plus, if reading's tricky, they can listen to the book being narrated! 
+            It’s a magical mix of creativity, tech, and fun!</h4>
+          <h3>Meet the team!</h3>
           <ul className="about-list-items">
             <li>
               <img src={Priya} alt="profile" className="profile-image" />
